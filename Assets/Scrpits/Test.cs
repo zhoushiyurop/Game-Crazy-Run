4cc06375c5b9295598650a75d299b4f86faa8527
zhoushiyurop <122235199@qq.com>
2018-11-04 19:33:46 +0800

    先注入xLua，不过第一个版本暂时不使用，先完成游戏基本功能

---
+++ Assets/Scrpits/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        XLua.LuaEnv luaEnv = new XLua.LuaEnv();
+        luaEnv.DoString("CS.UnityEngine.Debug.Log('hello world')");
+        luaEnv.Dispose();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
